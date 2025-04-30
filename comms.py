726c821caf8688889e714e0225c2f30dcbfb5dcd
Plu-sys <68435338+Plu-sys@users.noreply.github.com>
2025-04-30 15:56:12 +0200

    Update comms.py

--- comms.py
+++ comms.py
@@ -6,18 +6,30 @@ def messageHandler(client, userdata, message):
 def onconnect(client, userdata, flags, reason_code, properties):
     print("Client connection status: ", reason_code);
 
-client_id = "reciever"
+client_id = "reciever77"
+client_id2 = "reciever78"
+client1 = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id=client_id, protocol=MQTTProtocolVersion.MQTTv5, transport="tcp")
+client2 = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id=client_id2, protocol=MQTTProtocolVersion.MQTTv5, transport="tcp")
 
-client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2, client_id=client_id, protocol=MQTTProtocolVersion.MQTTv5, transport="tcp")
+client1.username_pw_set("robot_77_2","umbagDavgo")
+client2.username_pw_set("robot_78_2","tomCipveu")
 
-client.username_pw_set("robot_77_2","umbagDavgo")
-client.connect("mqtt.ics.ele.tue.nl", 1883, 60) # Address of the tue server
-client.on_connect = onconnect
-client.on_message = messageHandler
+client1.connect("mqtt.ics.ele.tue.nl", 1883, 60) # Address of the tue server
+client2.connect("mqtt.ics.ele.tue.nl", 1883, 60) # Address of the tue server
 
-client.subscribe("/pynqbridge/77/#",2)
+client1.on_connect = onconnect
+client1.on_message = messageHandler
+
+client2.on_connect = onconnect
+client2.on_message = messageHandler
+
+client1.subscribe("/pynqbridge/77/#",2)
+client2.subscribe("/pynqbridge/78/#",2)
 
 try:
-    client.loop_forever()   
+    client1.loop_forever() 
+    client2.loop_forever()  
 except:
+    client1.disconnect()
+    client2.disconnect()
     print("Comms ended")
